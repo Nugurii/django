b8a0a19ed7eab3b6df4415558d4eac6f5bf94823
韩佳乐 <1023538717@qq.com>
2020-05-13 19:29:01 +0800

    add bootstrap style and fix origin style

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
 class SignupForm(forms.Form):
-    username = forms.CharField(max_length=39, widget=forms.TextInput(attrs={'class': 'signup-form-control', 'autofocus': 'true', 'spellcheck': 'false', 'tabindex': '1', 'id': 'username'}))
-    password = forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'class': 'signup-form-control', 'autofocus': 'true', 'tabindex': '2', 'id': 'password'}))
+    username = forms.CharField(max_length=39, widget=forms.TextInput(attrs={'class': 'form-control signup-form-control', 'autofocus': 'true', 'spellcheck': 'false', 'tabindex': '1', 'id': 'username'}))
+    password = forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'class': 'form-control signup-form-control', 'autofocus': 'true', 'tabindex': '2', 'id': 'password'}))
 
 class SigninForm(forms.Form):
-    username = forms.CharField(max_length=39, widget=forms.TextInput(attrs={'class': 'signin-form-control', 'autofocus': 'autofocus', 'spellcheck': 'false', 'tabindex': '1', 'id': 'username'}), required=False)
-    password = forms.CharField(max_length=128,  widget=forms.PasswordInput(attrs={'class': 'signin-form-control', 'autofocus': 'autofocus', 'tabindex': '2', 'id': 'password'}), required=False)
+    username = forms.CharField(max_length=39, widget=forms.TextInput(attrs={'class': 'form-control signin-form-control', 'autofocus': 'autofocus', 'spellcheck': 'false', 'tabindex': '1', 'id': 'username'}), required=False)
+    password = forms.CharField(max_length=128,  widget=forms.PasswordInput(attrs={'class': 'form-control signin-form-control', 'autofocus': 'autofocus', 'tabindex': '2', 'id': 'password'}), required=False)
 
 class DateForm(forms.Form):
-    date = forms.CharField(widget=forms.TextInput(attrs={'class': 'home-form-control', 'autofocus': 'true', 'spellcheck': 'false'}), required=False)
+    date = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control home-form-control', 'autofocus': 'true', 'spellcheck': 'false'}), required=False)
