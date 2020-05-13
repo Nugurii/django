cf6a681b55a0b71a50a2b680934d95a21cd782ec
韩佳乐 <1023538717@qq.com>
2020-05-13 18:57:13 +0800

    delete some unused codes.

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from . import models
 from .forms import SigninForm, SignupForm, DateForm
 import json
 
-from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, HttpResponse
 
 def fib(n):
     if n == 0:
@@ -43,7 +43,6 @@ def home(request):
 def signin(request):
     print('1111')
     if request.method == "POST":
-        # reply = {'status': False, 'msg': None}
         message = None
         print('3434')
         print(fib(33)) # delay
@@ -53,25 +52,16 @@ def signin(request):
         if signin_form.is_valid():
             username = signin_form.cleaned_data['username']
             password = signin_form.cleaned_data['password']
-            # print(username, password)
             try:
                 user = models.User.objects.get(name=username)
                 if user.password == password:
                     request.session['is_signin'] = True
                     request.session['user_name'] = user.name
-                    # reply['status'] = True
-                    # reply['msg'] = 'Sign in successfully'
-                    # return HttpResponse(json.dumps(reply))
                     return redirect("/")
                 else:
-                    # reply['status'] = False
-                    # reply['msg'] = 'Incorrect password'
                     message = 'Incorrect password'
             except:
-                # reply['status'] = False
-                # reply['msg'] = 'Invalid username'
                 message = 'Invalid username'
-        # return HttpResponse(json.dumps(reply))
         return render(request, 'signin.html', {"signin_form": signin_form, "message": message})
     signin_form = SigninForm()
     return render(request, 'signin.html', {"signin_form": signin_form})
@@ -83,37 +73,25 @@ def signup(request):
             print(fib(33))
             signup_form = SignupForm(request.POST)
             if signup_form.is_valid():
-                # username = request.POST.get('username')
-                # password = request.POST.get('password')
                 username = signup_form.cleaned_data['username']
                 password = signup_form.cleaned_data['password']
-                print(username, password)
-                new_user = models.User.objects.create()
-                new_user.name = username
-                new_user.password = password
-                new_user.save()
-                request.session['is_signin'] = True
-                request.session['user_name'] = username
-                return redirect("/")
+                try:
+                    new_user = models.User.objects.create()
+                    new_user.name = username
+                    new_user.password = password
+                    new_user.save()
+                    return redirect("/user/signin/")
+                except:
+                    message = 'Error occured when creating account.'
+                    return render(request, 'signup.html', {"signup_form": signup_form, "message": message})
         else:
             reply = {'status': True, 'msg': None}
             print(fib(28))
             username = request.POST.get('username')
-            # action = request.POST.get('type')
-            # if action == "0":
             same_name_user = models.User.objects.filter(name=username)
             if same_name_user:
                 reply['status'] = False
             return HttpResponse(json.dumps(reply))
-        # elif action == "1":
-        #     print(fib(32))
-        #     password = request.POST.get('password')
-        #     new_user = models.User.objects.create()
-        #     new_user.name = username
-        #     new_user.password = password
-        #     new_user.save()
-        #     reply['status'] = True
-        #     return HttpResponse(json.dumps(reply))
     signup_form = SignupForm()
     return render(request, 'signup.html', {"signup_form": signup_form})
 
