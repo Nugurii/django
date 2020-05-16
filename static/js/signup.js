6197959eb5ccf59c728b35b48b05bf683de53cd8
韩佳乐 <1023538717@qq.com>
2020-05-16 10:00:46 +0800

    fix bugs which can trigger button click or enable by mistake.

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,13 +1,15 @@
-$(function () {
+$(function() {
 
     let is_username_valid = false;
     let is_password_valid = false;
-    
-    $("#username").on('input propertychange', function () {
+
+    $("#username").on('input propertychange', function() {
         let pattern = /^[a-zA-Z0-9]+$/;
         let username = $(this).val();
         let message = $("#username-message");
         message.hide();
+        is_username_valid = false;
+        $("#signup").attr("disabled", true);
         // console.log(username, username.length, pattern.test(username));
         $(this).removeClass("is-autocheck-errored");
         $(this).removeClass("is-autocheck-successed");
@@ -16,7 +18,6 @@ $(function () {
             $(this).removeClass("is-autocheck-loading");
             $(this).removeClass("is-autocheck-errored");
             $(this).removeClass("is-autocheck-successed");
-            is_username_valid = false;
             return;
         }
         if (pattern.test(username) == false) {
@@ -25,7 +26,6 @@ $(function () {
             $(this).removeClass("is-autocheck-successed");
             message.html("Username is not available.");
             message.show();
-            is_username_valid = false;
             return;
         }
         if (username.length < 5 || username.length > 39) {
@@ -34,7 +34,6 @@ $(function () {
             $(this).removeClass("is-autocheck-successed");
             message.html("Username is not available.");
             message.show();
-            is_username_valid = false;
             return;
         }
         let form_data = new FormData();
@@ -44,12 +43,12 @@ $(function () {
         $.ajax({
             url: '/user/signup/',
             type: 'POST',
-            headers: {"X-CSRFToken": $.cookie("csrftoken")},
+            headers: { "X-CSRFToken": $.cookie("csrftoken") },
             data: form_data,
             dataType: 'JSON',
             processData: false,
             contentType: false,
-            success: function (args) {
+            success: function(args) {
                 console.log(args.status, args.msg)
                 if (args.status) {
                     $(that).removeClass("is-autocheck-loading");
@@ -58,36 +57,39 @@ $(function () {
                     is_username_valid = true;
                     if (is_username_valid && is_password_valid)
                         $("#signup").removeAttr("disabled");
-                }
-                else {
+                    // console.log(username.length);
+                } else {
                     $(that).removeClass("is-autocheck-loading");
                     $(that).addClass("is-autocheck-errored");
                     $(that).removeClass("is-autocheck-successed");
                     message.html("Username has been taken.");
                     message.show();
                     is_username_valid = false;
+                    $("#signup").attr("disabled", true);
                 }
             }
         });
     })
 
-    $("#password").on('input propertychange', function () {
+    $("#password").on('input propertychange', function() {
         let password = $(this).val();
         let message = $("#password-message");
         message.hide();
+        is_password_valid = false;
+        $("#signup").attr("disabled", true);
         if (password == '') {
             return;
         }
         if (password.length < 8 || password.length > 128) {
             message.html("Password is not available.");
             message.show();
-            is_password_valid = false;
             return;
         }
         is_password_valid = true;
         if (is_username_valid && is_password_valid)
             $("#signup").removeAttr("disabled");
     })
+
 
     // $("#signup").click(function () {
     //     let form_data = new FormData();
@@ -122,13 +124,13 @@ $(function () {
     //     $(this).val("Signing up...")
     // })
 
-    $("#del-flash-message").click(function () {
+    $("#del-flash-message").click(function() {
         $("#flash-box").hide();
     })
 
-    $(document).keydown(function (event) {
+    $(document).keydown(function(event) {
         if (event.keyCode == 13) {
-            if ($("#username").is(":focus") || $("#password").is(":focus") || $("#signup").is(":focus")){
+            if (($("#username").is(":focus") || $("#password").is(":focus") || $("#signup").is(":focus")) && ($("#signup").prop("disabled") == false)) {
                 $("#signup").click();
                 return false;
             }
